694eda7a45f406c4753e1673f07ae690e6b1c2ba
njw0703 <sjkjw09@gmail.com>
2026-07-24 20:22:07 +0900

    프론트엔트 페이지 구조변경 및 생성

---
+++ frontend/src/pages/historypage.tsx
@@ -0,0 +1,5 @@
+function HistoryPage() {
+  return <h1>분석 이력 페이지</h1>;
+}
+
+export default HistoryPage;
---
+++ frontend/src/pages/resultpage.tsx
@@ -0,0 +1,5 @@
+function ResultPage() {
+  return <h1>분석 결과 페이지</h1>;
+}
+
+export default ResultPage;
