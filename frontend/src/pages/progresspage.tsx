bca6c81287397ab97a74530bc79bad397cbe0fe7
njw0703 <sjkjw09@gmail.com>
2026-07-24 20:48:21 +0900

    프론트엔트 페이지 구조변경 및 생성 (#4)
    프론트엔드 페이지 구조변경 및 생성

---
+++ frontend/src/pages/progresspage.tsx
@@ -0,0 +1,5 @@
+function ProgressPage() {
+  return <h1>분석 진행 페이지</h1>;
+}
+
+export default ProgressPage;
---
+++ frontend/src/pages/uploadpage.tsx
@@ -0,0 +1,5 @@
+function UploadPage() {
+  return <h1>CT 업로드 페이지</h1>;
+}
+
+export default UploadPage;
