7b4c941b867ffbb26b28ea96b0a74323bac9a17a
choi-creator07 <choi.yurim9807@gmail.com>
2026-07-26 12:39:32 +0900

    feat: add MOSEC gateway integration

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -96,11 +96,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "medical_cdss",
-        "USER": "postgres",
-        "PASSWORD": "leehan1020",   # 깃에 올리기 전에 바꾸기
-        "HOST": "localhost",
-        "PORT": "5432",
+        "NAME": os.getenv("DB_NAME", "medical_cdss"),
+        "USER": os.getenv("DB_USER", "postgres"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST", "localhost"),
+        "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
 
