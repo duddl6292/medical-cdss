5b80ad528084eafe988e1524b80b309284097cad
choi-creator07 <choi.yurim9807@gmail.com>
2026-07-27 13:44:32 +0900

    chore: log backend request failures

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -270,3 +270,20 @@ MAX_NIFTI_UPLOAD_BYTES = int(
 # 새 모델에서 별도 기본키를 지정하지 않으면 BigAutoField를 사용합니다.
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django.request": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False,
+        },
+    },
+}
